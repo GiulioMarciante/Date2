64bc4c5a2eca7f4a5e4f9bc8914ca696537ebee7
Giulio Marciante <87944393+undeermind@users.noreply.github.com>
2023-12-01 12:32:15 +0100

    Esercizio Date2

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+//Crea un oggetto OffsetDateTime da questa stringa 2023-03-01T13:00:00Z
+//Formatta la data ottenendo 01 marzo 2023
+//Stampa sulla console
+public class Main {
+    public static void main(String[] args) {
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+
+        String dataStringMedium = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
+        System.out.println("Medium: " + dataStringMedium);
+    }
+}
